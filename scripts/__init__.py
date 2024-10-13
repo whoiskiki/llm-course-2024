3da2e748860f947c6efd42d2554e4b6a30bc2967
whoiskiki <severnayapol@mail.ru>
2024-10-13 19:10:43 +0300

    help

---
+++ scripts/__init__.py
@@ -0,0 +1,7 @@
+from .tokenizer import BpeTokenizer
+from .model import Model
+from .dataset import MyDataset
+from .collator import Collator
+from .trainer import Trainer
+from .generation import generate
+
